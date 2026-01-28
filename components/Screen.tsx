60b71f708ee20bb37efa9e91e3c75234bd99c9d4
rsrohitsingh511 <rsrohitsingh511@gmail.com>
2026-01-28 16:46:08 +0530

    feat: Configure initial app settings, integrate EAS build, and update `Screen` component for `SafeAreaProvider`.

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import React from "react";
 import { Platform, StatusBar, View, ViewProps } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { twMerge } from "tailwind-merge";
 
 function cn(...inputs: ClassValue[]) {
@@ -20,7 +20,7 @@ export const Screen: React.FC<ScreenProps> = ({
     className,
     ...props
 }) => {
-    const Container = safeArea ? SafeAreaView : View;
+    const Container = safeArea ? SafeAreaProvider : View;
 
     return (
         <Container
